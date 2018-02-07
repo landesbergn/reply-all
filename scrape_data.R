89ad7fef2668a6abd2dc1cf57f8579ec4dd30ec4
landesbergn <noah.landesberg@gmail.com>
2018-02-06 23:48:33 -0500

    big changes to the reply all website has forced some refactoring, but getting better at rvest so that helps

--- scrape_data.R
+++ scrape_data.R
@@ -97,3 +97,80 @@ reply_all_sentiment <- transcript_tidy %>%
 ggplot(reply_all_sentiment, aes(index, sentiment)) +
   geom_bar(stat = "identity", show.legend = FALSE) +
   theme_minimal()
+
+########## It was at this point... gimlet changed their web pages ########
+# base url
+reply_all_url <- "https://www.gimletmedia.com/reply-all/all#all-episodes-list"
+
+# get episode titles
+episode_names <- reply_all_url %>%
+  read_html() %>%
+  html_nodes("a") %>%
+  html_attr("href") %>%
+  data.frame()
+
+colnames(episode_names) <- "episode"
+
+episode_names <- episode_names %>%
+  filter(
+    str_detect(episode, "episode-player"),
+    !str_detect(episode, "re-broadcast"),
+    !str_detect(episode, "rebroadcsast"),
+    !str_detect(episode, "presents"),
+    !str_detect(episode, "introducing")
+  ) %>%
+  distinct()
+
+ep_data <- episode_names %>%
+  mutate(
+    episode_link = paste0("https://www.gimletmedia.com", episode)
+  )
+
+transcript <- ep_data$episode_link[1] %>%
+  read_html() %>%
+  html_nodes(".episode-transcript") %>%
+  html_text() %>%
+  str_split("\\\n") %>%
+  data.frame(stringsAsFactors = FALSE)
+
+colnames(transcript) <- "line"
+
+transcript$line <- transcript$line %>%
+  str_replace_all("[\\\n]", "") %>%
+  str_replace_all("[\\\t]", "") %>%
+  str_trim()
+
+transcript_new <- transcript %>%
+  filter(
+    nchar(line) > 0,
+    !str_detect(line, "^\\[")
+  ) %>%
+  mutate(
+    linenumber = row_number(),
+    speaker = str_extract(line, "^[[:upper:]]+"),
+    line = str_replace_all(line, "^[[:upper:]]+ [[:upper:]]+:", ""),
+    text = str_replace_all(line, "^[[:upper:]]+:", "")
+  ) %>%
+  select(speaker, text, linenumber)
+
+transcript_tidy <- transcript_new %>%
+  unnest_tokens(word, text)
+
+data("stop_words")
+transcript_clean <- transcript_tidy %>%
+  anti_join(stop_words)
+
+transcript_clean %>%
+  count(word, sort = TRUE)
+
+bing <- get_sentiments("bing")
+
+reply_all_sentiment <- transcript_tidy %>%
+  inner_join(bing) %>%
+  count(index = linenumber %/% 1, sentiment) %>%
+  tidyr::spread(sentiment, n, fill = 0) %>%
+  mutate(sentiment = positive - negative)
+
+ggplot(reply_all_sentiment, aes(index, sentiment)) +
+  geom_bar(stat = "identity", show.legend = FALSE) +
+  theme_minimal()
